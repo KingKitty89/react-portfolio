7e1c81a7d54587a47d22270399e3e8d577093b74
Jessica Helft <jessicahelft@gmail.com>
2020-07-25 18:01:09 -0500

    added particles animation to homepage

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,65 @@
-import React from 'react';
-import NavBar from  "./Navbar";
+import React from "react";
+import NavBar from "./Navbar";
 import Header from "./Header";
+import Particles from "react-particles-js";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  particlesCanvas: {
+    position: "absolute",
+    opacity: "0.3"
+  },
+});
 
 const Home = () => {
-    return (
-        <div>
-          <NavBar /> 
-          <Header />
-        </div>
-    )
-}
+  const classes = useStyles();
+  return (
+    <>
+      <NavBar />
+      <Header />
+      <Particles
+        canvasClassName={classes.particlesCanvas}
+        params={{
+          particles: {
+            number: {
+              value: 45,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 1,
+                color: "tomato",
+              },
+            },
+            size: {
+              value: 8,
+              random: true,
+              animation: {
+                enable: true,
+                speed: 1,
+                size_min: 0.1,
+                sync: true,
+              },
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              animation: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: true
+              },
+            },
+          },
+        }}
+      />
+    </>
+  );
+};
 
-export default Home
+export default Home;

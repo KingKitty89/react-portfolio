1f9f15b70790328d1b9dd62e3fa5c698fb7f6b63
Jessica Helft <jessicahelft@gmail.com>
2020-07-25 22:16:27 -0500

    added a resume timeline page with work experience placeholders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { Route } from "react-router-dom"
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Home from "./components/";
+import Resume from "./components/Resume";
 import "./App.css";
+
 
 function App() {
   return (
     <>
     <CssBaseline />
-      <Home />
+      <Route exact path="/" component={Home}/>
+      <Route path="/resume" component={Resume}/>
     </>
   );
 }

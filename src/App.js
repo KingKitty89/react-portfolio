6637f83c5d79b6953d5c5b705c9f7f75d0b76157
Jessica Helft <jessicahelft@gmail.com>
2020-07-25 22:26:00 -0500

    portfolio component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom"
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Home from "./components/";
 import Resume from "./components/Resume";
+import Portfolio from "./components/Portfolio";
 import "./App.css";
 
 
@@ -12,6 +13,7 @@ function App() {
     <CssBaseline />
       <Route exact path="/" component={Home}/>
       <Route path="/resume" component={Resume}/>
+      <Route path="/portfolio" component={Portfolio}/>
     </>
   );
 }

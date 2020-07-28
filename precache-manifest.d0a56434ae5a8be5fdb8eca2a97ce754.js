f236816b86a3a0f7c8bc61e5d7eeddef6c762f3e
Jessica Helft <jessicahelft@gmail.com>
2020-07-27 23:24:47 -0500

    Updates

---
+++ precache-manifest.d0a56434ae5a8be5fdb8eca2a97ce754.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37996a581999961f03909bd4cff05427",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "160eefa81e922c9a0295",
+    "url": "/react-portfolio/static/css/main.0e192901.chunk.css"
+  },
+  {
+    "revision": "74a69ac3abd12242b0dc",
+    "url": "/react-portfolio/static/js/2.277db9cd.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/react-portfolio/static/js/2.277db9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160eefa81e922c9a0295",
+    "url": "/react-portfolio/static/js/main.c41d2325.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "e7c73aff25d5a6d98bb7cb1145822909",
+    "url": "/react-portfolio/static/media/ resume.e7c73aff.pdf"
+  },
+  {
+    "revision": "a06f42431c54fed1f0408678583af9b9",
+    "url": "/react-portfolio/static/media/Eat-Da-Burger.a06f4243.gif"
+  },
+  {
+    "revision": "e571ae6db17e126b9a3559fb1dd52caf",
+    "url": "/react-portfolio/static/media/avatar.e571ae6d.png"
+  },
+  {
+    "revision": "d6c1091df459e27aa9b38d408fb99799",
+    "url": "/react-portfolio/static/media/back.d6c1091d.jpeg"
+  },
+  {
+    "revision": "c610844a54cfa2b2d30372f08b70497a",
+    "url": "/react-portfolio/static/media/dayplannergif.c610844a.gif"
+  },
+  {
+    "revision": "4caa4880f212325992d1d87b505558cc",
+    "url": "/react-portfolio/static/media/onhandgif.4caa4880.gif"
+  },
+  {
+    "revision": "8580a660856692525f8d6fb71484a195",
+    "url": "/react-portfolio/static/media/passwordgengif.8580a660.gif"
+  },
+  {
+    "revision": "b49ed9153083212e9d386ad0ffaac782",
+    "url": "/react-portfolio/static/media/simplysomm.b49ed915.gif"
+  },
+  {
+    "revision": "330a37c924f43ef9c3d8dec6ff5fa9fe",
+    "url": "/react-portfolio/static/media/teamgen.330a37c9.gif"
+  }
+]);

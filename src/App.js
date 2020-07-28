ca01c4bc9dc74334084c05b6ca6663967f622b7a
Jessica Helft <jessicahelft@gmail.com>
2020-07-28 00:12:27 -0500

    wah

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom"
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Home from "./components/react-portfolio";
+import Home from "./components/";
 import Resume from "./components/Resume";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
     <CssBaseline />
-      <Route exact path="/react-portfolio" component={Home}/>
+      <Route exact path="/" component={Home}/>
       <Route path="/resume" component={Resume}/>
       <Route path="/portfolio" component={Portfolio}/>
       <Route path="/contact" component={Contact}/>
